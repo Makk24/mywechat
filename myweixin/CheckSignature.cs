474209127b609b4cf9d131a7c48aa314913f0c9e
mywebchat <1009576562@qq.com>
2016-08-15 21:55:53 +0800

    添加项目文件。

---
+++ myweixin/CheckSignature.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+namespace myweixin
+{
+    public class CheckSignature
+    {
+        /// <summary>
+        /// 默认Token
+        /// </summary>
+        public const string Token = "makkweixin";
+
+        /// <summary>
+        /// 检查签名是否正确
+        /// </summary>
+        /// <param name="signature"></param>
+        /// <param name="timestamp"></param>
+        /// <param name="nonce"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static bool Check(string signature, string timestamp, string nonce, string token)
+        {
+            return signature == GetSignaTure(timestamp, nonce, token);
+        }
+        /// <summary>
+        /// 返回正确的签名
+        /// </summary>
+        /// <param name="timestamp"></param>
+        /// <param name="nonce"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static string GetSignaTure(string timestamp, string nonce, string token = Token)
+        {
+            token = token ?? Token;
+            string[] arr = new[] { token, timestamp, nonce }.OrderBy(z => z).ToArray();
+            string arrString = string.Join("", arr);
+            string enText = FormsAuthentication.HashPasswordForStoringInConfigFile(arrString, "SHA1").ToLower();
+            return enText;
+        }
+    }
+}
---
+++ myweixin/index.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace myweixin
+{
+    public partial class index : System.Web.UI.Page
+    {
+        private readonly String Token = "weixinmakk";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Auth();
+        }
+        private void Auth()
+        {
+            string signature = QueryString("signature");
+            string timestamp = QueryString("timestamp");
+            string nonce = QueryString("nonce");
+            string echostr = QueryString("echostr");
+            if (Request.HttpMethod == "GET")
+            {
+                if (CheckSignature.Check(signature, timestamp, nonce, Token))
+                {
+                    WriteContent(echostr);
+                }
+                else
+                {
+                    WriteContent("failed:" + signature + "," + CheckSignature.GetSignaTure(timestamp, nonce, Token) + "." + "如果你在浏览器里看到这句话，说明此地址可以被作为微信公共账号后台的URL，请注意保持Token一致。");
+                }
+            }
+        }
+        private void WriteContent(string str)
+        {
+            Response.Output.Write(str);
+        }
+        private string QueryString(string name)
+        {
+            if (string.IsNullOrWhiteSpace(Request[name]))
+            {
+                return HttpContext.Current.Request.QueryString["signature"];
+            }
+            else
+            {
+                return Request[name];
+            }
+        }
+    }
+}
